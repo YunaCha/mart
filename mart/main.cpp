078f12027174a48961bbfe292f83132cc2bfb2ac
YousungCha <yscha08@gmail.com>
2018-11-07 10:54:45 +0900

    Add : Clock Function

--- mart/main.cpp
+++ mart/main.cpp
@@ -1,10 +1,35 @@
 #include "myFunc.h"
+#define NORMAL_MODE 0
+#define MENU_MODE 1
+#define CALC_MODE 2
+#define TOTAL_MODE 3
+
+unsigned int tCount = 0, hou = 0, min = 0, sec = 0;
+int mode = NORMAL_MODE;
+
+ISR(TIMER0_OVF_vect)
+{
+	tCount++;
+	if (tCount >= 61)
+	{
+		// 1 second
+		sec++;
+		if (sec > 59) { min++; sec = 0; }
+		if (min > 59) { hou++; min = 0; }
+		if (hou > 23) { hou = min = sec = 0; }
+		tCount = 0;
+	}
+}
 
 int main(void)
 {
 	initSystem();
     while (1) 
     {
+		if (mode == NORMAL_MODE)
+		{
+			DisplayClock(hou,min,sec);
+		}
     }
 }
 
--- mart/myFunc.h
+++ mart/myFunc.h
@@ -5,6 +5,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 #include "lcd.h"
 
 void initSystem()
@@ -12,6 +13,9 @@ void initSystem()
 	// Register Setting
 	DDRA = 0xff;	// 0,1 : Motor | 5,6,7 : RGY LED
 	DDRD = 0x0f;	// 0,1,2,3 : Row 0, 1, 2, 3 | 4,5,6,7 : Col 4, 5, 6, 7
+	TIMSK = 0x01;
+	TCCR0 = 0x07;	// 1024분주
+	
 	lcd_init();
 	lcd_clear();
 	for (int i=0; i<2; i++)
@@ -23,7 +27,16 @@ void initSystem()
 		lcd_clear();
 		PORTA = 0b11100000;
 		_delay_ms(100);
-	}	
+	}		
+	sei();	// Global Interrupt Enable
+}
+
+void DisplayClock(unsigned int hou, unsigned int min, unsigned sec)
+{
+	char str[16];
+	sprintf(str,"    %02d:%02d:%02d",hou,min,sec);
+	lcd_putsf(0,0,(unsigned char *)"    WELCOME!!   ");
+	lcd_putsf(0,1,(unsigned char *)str);	
 }
 
 #endif
